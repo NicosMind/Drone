7ca80a7ca676d3e92327ed9d4a4f5854ec02233a
Nico <nico_drift@hotmail.fr>
2020-03-24 14:18:40 +0100

    full clean-up

--- Drone.ino
+++ Drone.ino
@@ -33,174 +33,171 @@ int CW;
 int CCW;
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
-    BlueT.begin(38400);
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	BlueT.begin(38400);
 
-    Serial.println("ENTER AT Commands:");
-    
-    pinMode(Moteur_A, OUTPUT);
-    pinMode(Moteur_B, OUTPUT);
-    pinMode(ENA, OUTPUT);
-    
-    pinMode(trigPin, OUTPUT); 
-    pinMode(echoPin, INPUT); 
+	Serial.println("ENTER AT Commands:");
 
-    Serial.begin(9600);
-    pinMode(leftSensor, INPUT);
-    pinMode(middleSensor, INPUT);
-    pinMode(rightSensor, INPUT);
+	pinMode(Moteur_A, OUTPUT);
+	pinMode(Moteur_B, OUTPUT);
+	pinMode(ENA, OUTPUT);
 
-    ServoDir.attach(9);
-    ServoDir.write(90);
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+
+	Serial.begin(9600);
+	pinMode(leftSensor, INPUT);
+	pinMode(middleSensor, INPUT);
+	pinMode(rightSensor, INPUT);
+
+	ServoDir.attach(9);
+	ServoDir.write(90);
 }
 
-void loop() {
-
-    
-    if (proximitySensor() < securityDistance)
-    {
-        digitalWrite(Moteur_A, HIGH);
-        digitalWrite(Moteur_B, LOW);
-        analogWrite(ENA, 255);
-    }
-    else
-    {
-        bluetoothControl();
-    }
-   
+void loop()
+{
+	if (proximitySensor() < securityDistance)
+	{
+		digitalWrite(Moteur_A, HIGH);
+		digitalWrite(Moteur_B, LOW);
+		analogWrite(ENA, 255);
+	}
+	else
+	{
+		bluetoothControl();
+	}
 }
 
 void powerMotor(int rotation)
 {
-    if (rotation = CW)
-    {
-        digitalWrite(Moteur_A, HIGH);
-        digitalWrite(Moteur_B, LOW);
-    }
-    else
-    {
-        digitalWrite(Moteur_A, LOW);
-        digitalWrite(Moteur_B, HIGH);
-    }
-    analogWrite(ENA, 255);
+	if (rotation = CW)
+	{
+		digitalWrite(Moteur_A, HIGH);
+		digitalWrite(Moteur_B, LOW);
+	}
+	else
+	{
+		digitalWrite(Moteur_A, LOW);
+		digitalWrite(Moteur_B, HIGH);
+	}
+	analogWrite(ENA, 255);
 }
 
 void direction(int angle)
 {
-    ServoDir.write(angle);
+	ServoDir.write(angle);
 }
 
 void bluetoothControl()
 {
-    if (BlueT.available()) {
-        message = BlueT.read();
-        Serial.print(message);
-        switch (message) {
+	if (BlueT.available()) {
+		message = BlueT.read();
+		Serial.print(message);
+		switch (message) {
 
-        case 'X':
-            PWM = BlueT.parseInt();
-            if (PWM > 131) {
-                MoteurSpeed = map(PWM, 128, 255, 0, 255);
-                Serial.println(PWM);
-                Serial.println(MoteurSpeed);
-                digitalWrite(Moteur_A, LOW);
-                digitalWrite(Moteur_B, HIGH);
-                analogWrite(ENA, MoteurSpeed);
-            }
-            else if (PWM < 125) {
-                MoteurSpeed = map(PWM, 128, 0, 0, 255);
-                Serial.println(PWM);
-                Serial.println(MoteurSpeed);
-                digitalWrite(Moteur_A, HIGH);
-                digitalWrite(Moteur_B, LOW);
-                analogWrite(ENA, MoteurSpeed);
-            }
-            else if (125 < PWM < 131) {
-                MoteurSpeed = 0;
-                digitalWrite(Moteur_A, LOW);
-                digitalWrite(Moteur_B, LOW);
-                analogWrite(ENA, MoteurSpeed);
-            }
-            break;
+		case 'X':
+			PWM = BlueT.parseInt();
+			if (PWM > 131) {
+				MoteurSpeed = map(PWM, 128, 255, 0, 255);
+				Serial.println(PWM);
+				Serial.println(MoteurSpeed);
+				digitalWrite(Moteur_A, LOW);
+				digitalWrite(Moteur_B, HIGH);
+				analogWrite(ENA, MoteurSpeed);
+			}
+			else if (PWM < 125) {
+				MoteurSpeed = map(PWM, 128, 0, 0, 255);
+				Serial.println(PWM);
+				Serial.println(MoteurSpeed);
+				digitalWrite(Moteur_A, HIGH);
+				digitalWrite(Moteur_B, LOW);
+				analogWrite(ENA, MoteurSpeed);
+			}
+			else if (125 < PWM < 131) {
+				MoteurSpeed = 0;
+				digitalWrite(Moteur_A, LOW);
+				digitalWrite(Moteur_B, LOW);
+				analogWrite(ENA, MoteurSpeed);
+			}
+			break;
 
 
-        case 'Y':
-            PWM = BlueT.parseInt();
-            PWM = map(PWM, 0, 255, 0, 180);
-            PWM = constrain(PWM, 30, 150);
-            ServoDir.write(PWM);
-            delay(15);
-            break;
+		case 'Y':
+			PWM = BlueT.parseInt();
+			PWM = map(PWM, 0, 255, 0, 180);
+			PWM = constrain(PWM, 30, 150);
+			ServoDir.write(PWM);
+			delay(15);
+			break;
 
-        case 'F':
-            lineFollower();
-
-        }
-    }
+		case 'F':
+			lineFollower();
+			break;
+		}
+	}
 }
 
 int proximitySensor()
 {
-
-        digitalWrite(trigPin, LOW);
-        delayMicroseconds(2);
-        digitalWrite(trigPin, HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trigPin, LOW);
-        duration = pulseIn(echoPin, HIGH);
-        distance = duration * 0.034 / 2;
-        Serial.print("Distance: ");
-        Serial.println(distance);
-        return distance;
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+	duration = pulseIn(echoPin, HIGH);
+	distance = duration * 0.034 / 2;
+	Serial.print("Distance: ");
+	Serial.println(distance);
+	return distance;
 }
 
 void lineFollower()
 {
-        stateLeftSensor = digitalRead(leftSensor);
-        stateMiddleSensor = digitalRead(middleSensor);
-        stateRightSensor = digitalRead(rightSensor);
+	stateLeftSensor = digitalRead(leftSensor);
+	stateMiddleSensor = digitalRead(middleSensor);
+	stateRightSensor = digitalRead(rightSensor);
 
-        if (stateMiddleSensor) 
-        {
-            if ((stateLeftSensor) && (!stateRightSensor))
-            {
-                Serial.println("Tourner à gauche");
-                powerMotor(CW);
-                direction(0);
-            }
-            else if ((!stateLeftSensor) && (stateRightSensor)) 
-            {
-                Serial.println("Tourner à droite");
-                powerMotor(CW);
-                direction(180);
-            }
-            else 
-            {
-                Serial.println("Continuer tout droit");
-                powerMotor(CW);
-                direction(90);
-            }
-        }
-        else 
-        {
-            if ((stateLeftSensor) && (!stateRightSensor)) 
-            {
-                Serial.println("Tourner à gauche");
-                powerMotor(CW);
-                direction(0);
-            }
-            else if ((!stateLeftSensor) && (stateRightSensor)) 
-            {
-                Serial.println("Tourner à droite");
-                powerMotor(CW);
-                direction(180);
-            }
-            else 
-            {
-                Serial.println("Reculer");
-                powerMotor(CCW);
-                direction(90);
-            }
-        }
+	if (stateMiddleSensor)
+	{
+		if ((stateLeftSensor) && (!stateRightSensor))
+		{
+			Serial.println("Tourner à gauche");
+			powerMotor(CW);
+			direction(0);
+		}
+		else if ((!stateLeftSensor) && (stateRightSensor))
+		{
+			Serial.println("Tourner à droite");
+			powerMotor(CW);
+			direction(180);
+		}
+		else
+		{
+			Serial.println("Continuer tout droit");
+			powerMotor(CW);
+			direction(90);
+		}
+	}
+	else
+	{
+		if ((stateLeftSensor) && (!stateRightSensor))
+		{
+			Serial.println("Tourner à gauche");
+			powerMotor(CW);
+			direction(0);
+		}
+		else if ((!stateLeftSensor) && (stateRightSensor))
+		{
+			Serial.println("Tourner à droite");
+			powerMotor(CW);
+			direction(180);
+		}
+		else
+		{
+			Serial.println("Reculer");
+			powerMotor(CCW);
+			direction(90);
+		}
+	}
 }
